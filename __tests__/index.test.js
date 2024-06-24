d1d8a9a6af3588995d2027c6221f86da39438f48
Aleksandr <0rendl050@gmail.com>
2024-06-24 23:57:44 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,8 +8,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('plain object', () => {
-  const file1 = path.join('__fixtures__', 'file1.json');
-  const file2 = path.join('__fixtures__', 'file2.json');
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
   const plainData = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
   expect(gendiff(file1, file2)).toEqual(plainData);
 });
